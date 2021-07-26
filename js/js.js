e2d430966ee27fcfdcf5a499443c39134f63229f
Wissamtalal9 <talalwissam5@gmail.com>
2021-07-26 21:24:02 +0300

    javascript and make about me page

---
+++ js/js.js
@@ -0,0 +1,65 @@
+'use strict'
+let namee = prompt('what is your name');
+namee.toLowerCase();
+
+let country = prompt('where are you from' ) ;
+country.toLowerCase();
+let major = prompt('what is your main major' ) ;
+major.toLowerCase();
+let mark = prompt('what the mark you have it in your major' ) ; 
+mark.toLowerCase();
+
+alert('you are welcome ' + namee +  ' you are from ' + country + ', your main major is : ' + major + ' and you get mark in your major : ' + mark ) 
+
+
+console.log(' youser name is : ' + namee) ;
+console.log('your are from ' + country);
+console.log('your main major is  ' + major);
+console.log('you ger mark in your main major ' + mark);
+
+function myFunction() {
+    document.getElementById("demo").innerHTML = "yes";
+  }
+  function myFunction1() {
+    document.getElementById("demo").innerHTML = "no";
+  }
+
+
+
+  function myFunction2() {
+    document.getElementById("demo2").innerHTML = "yes";
+  }
+  function myFunction22() {
+    document.getElementById("demo2").innerHTML = "no";
+  }
+
+
+
+  function myFunction3() {
+    document.getElementById("demo3").innerHTML = "yes";
+  }
+  function myFunction33() {
+    document.getElementById("demo3").innerHTML = "no";
+  }
+  
+
+
+  function myFunction4() {
+    document.getElementById("demo4").innerHTML = "yes";
+
+  }
+  function myFunction44() {
+    document.getElementById("demo4").innerHTML = "no";
+  }
+
+
+
+  function myFunction5() {
+    document.getElementById("demo5").innerHTML = "yes";
+  }
+  function myFunction55() {
+    document.getElementById("demo5").innerHTML = "no";
+  }
+  function myFunctionname() {
+    document.getElementById("name").innerHTML = "Welcome to my website "+ namee;
+  }
